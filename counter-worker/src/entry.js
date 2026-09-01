c26d3ce39b9fbed597f2fe27ffb21f366e96cac0
ISASTO <54598558+ISASTO@users.noreply.github.com>
2026-09-01 07:13:46 -0500

    Route mode-specific leaderboard API

--- counter-worker/src/entry.js
+++ counter-worker/src/entry.js
@@ -1,8 +1,11 @@
 import worker from "./index.js";
 import { handleEnhancedRequest } from "./insights.js";
+import { handleLeaderboardRequest } from "./leaderboards.js";
 
 export default {
   async fetch(request, env, ctx) {
+    const leaderboardResponse = await handleLeaderboardRequest(request, env);
+    if (leaderboardResponse) return leaderboardResponse;
     return handleEnhancedRequest(request, env, ctx, worker);
   },
 };
