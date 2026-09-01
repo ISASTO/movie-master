4a446373132b4cd0197b182dba5e59cf99be8298
ISASTO <54598558+ISASTO@users.noreply.github.com>
2026-09-01 07:14:30 -0500

    Load split mode leaderboards

--- analytics/auto-refresh.js
+++ analytics/auto-refresh.js
@@ -16,4 +16,14 @@
   sourceBreakdown.src = "./source-breakdown.js?v=20260901-1";
   sourceBreakdown.defer = true;
   document.head.append(sourceBreakdown);
+
+  const leaderboardStyles = document.createElement("link");
+  leaderboardStyles.rel = "stylesheet";
+  leaderboardStyles.href = "./mode-leaderboards.css?v=20260901-1";
+  document.head.append(leaderboardStyles);
+
+  const modeLeaderboards = document.createElement("script");
+  modeLeaderboards.src = "./mode-leaderboards.js?v=20260901-1";
+  modeLeaderboards.defer = true;
+  document.head.append(modeLeaderboards);
 })();
